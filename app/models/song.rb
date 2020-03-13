6effaa57c47b0f25e879f986d7105fac118a062a
justin <justinphil02@yahoo.com>
2020-03-12 22:44:25 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,6 @@
 class Song < ApplicationRecord
     include ActiveModel::Validations
     validates_with SongValidator
-    validates :artist_name, presence: true
 end
 
 
--- app/validators/song_validator.rb
+++ app/validators/song_validator.rb
@@ -1,5 +1,5 @@
 #to do - custom validation 
-#Title -C
+#Title 
 #cannot be blank and 
 #not repeated by the same artist in the same year
 
@@ -11,14 +11,37 @@
 
 class SongValidator < ActiveModel::Validator 
     def validate(record)
+        # Title Validation
+        unless record.title.class == String && record.title.length > 0 
+            record.errors[:title] << "Song title cannot be blank"
+        end 
+
         song_exists = Song.find_by(title: record.title, release_year: record.release_year, artist_name: record.artist_name)
         if song_exists
             record.errors[:title] << "Song title cannot be repeated by the same artist in the same year."
         end 
 
+        #Released Validation
         unless record.released.class == TrueClass ||  record.released.class == FalseClass
             record.errors[:released] << "Released must be either True or False"
         end 
-        
+
+        #Release Year Validation
+        if record.released == true 
+            unless record.release_year.class == Integer
+                record.errors[:release_year] << "If the song is released it must have a release year"
+            end 
+
+            this_year = Time.new.year 
+            if record.release_year && record.release_year > this_year
+                record.errors[:release_year] << "The release date of a song cannot be a future year"
+            end      
+           
+            #Artist can only release one song per year
+            #artist_already_released_a_song = Song.find_by(release_year: record.release_year, artist_name: record.artist_name)
+            #if artist_already_released_a_song
+            #    record.errors[:release_year] << "An artist can only release one song per year"
+            #end 
+        end 
     end 
 end 
